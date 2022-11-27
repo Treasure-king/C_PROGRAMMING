4bb2828ed8627fe9e4f7844539d7cba2fc337780
Treasure-king <107706054+Treasure-king@users.noreply.github.com>
2022-11-27 21:39:15 +0530

    Add files via upload
    matrix solution

---
+++ source_codes/matrix.c
@@ -0,0 +1,63 @@
+////IT IS A SIMPLE 2*2 AND 3*3 MATRIX SOLVING SYSTEM 
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//#include<math.h>
+//void two() {
+//	int a, b, c, d;
+//	printf("enter 1st number= ");
+//	scanf("%d", &a);
+//	printf("\nenter 2nd number= ");
+//	scanf("%d", &b);
+//	printf("\nenter 3rd number= ");
+//	scanf("%d", &c);
+//	printf("\nenter 4th number= ");
+//	scanf("%d", &d);
+//	printf("\n\t\t\t\t\t| %d  %d |", a, b);
+//	printf("\n\t\t\t\t\t| %d  %d |", c, d);
+//	system("color E4");
+//	printf("\n\n\n\t\t\t\t\tTHE SOLUTION OF GIVEN MATRIX IS= %d\n\n\n", (a * d) - (c * b));
+//}
+//void three() {
+//	int a, b, c, d, e, f, g, h, i;
+//	printf("enter 1st number= ");
+//	scanf("%d", &a);
+//	printf("\nenter 2nd number= ");
+//	scanf("%d", &b);
+//	printf("\nenter 3rd number= ");
+//	scanf("%d", &c);
+//	printf("\nenter 4th number= ");
+//	scanf("%d", &d);
+//	printf("\nenter 5rd number= ");
+//	scanf("%d", &e);
+//	printf("\nenter 6th number= ");
+//	scanf("%d", &f);
+//	printf("\nenter 7th number= ");
+//	scanf("%d", &g);
+//	printf("\nenter 8rd number= ");
+//	scanf("%d", &h);
+//	printf("\nenter 9th number= ");
+//	scanf("%d", &i);
+//	printf("\n\t\t\t\t\t| %d  %d  %d |", a, b, c);
+//	printf("\n\t\t\t\t\t| %d  %d  %d |", d, e, f);
+//	printf("\n\t\t\t\t\t| %d  %d  %d |", g, h, i);
+//	system("color E4");
+//	printf("\n\n\n\t\t\t\t\tTHE SOLUTION OF GIVEN MATRIX IS= %d\n\n\n", a*(e*i-h*f)-b*(d*i-g*f)+c*(d*h-g*e));
+//}
+//void main() {
+//	int a;
+//	printf("what matrix you want to solve");
+//	printf("\n1.) 2*2");
+//	printf("\n2.) 3*3");
+//	printf("\npress 1 or 2= ");
+//	scanf("%d", &a);
+//	if (a == 1)
+//	{
+//		two();
+//	}
+//	else if (a == 2)
+//	{
+//		three();
+//	}
+//	else
+//		printf("please enter the valid command");
+//}
